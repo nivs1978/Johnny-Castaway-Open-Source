ea56feca99ed0d5324688e47cd2e43618729105e
Hans Milling <nivs1978@gmail.com>
2015-12-18 10:13:52 +0100

    Added GPL license notification to the top of the files.

--- JCOS/ResourceManager.cs
+++ JCOS/ResourceManager.cs
@@ -1,4 +1,20 @@
-﻿using System;
+﻿/*
+ResourceManager.cs
+
+This file is part of Johnny Castaway Open Source.
+
+Copyright (c) 2015 Hans Milling
+
+Johnny Castaway Open Source is free software: you can redistribute it and/or modify it under the terms of the
+GNU General Public License as published by the Free Software Foundation, either version 3 of the License,
+or (at your option) any later version.
+Johnny Castaway Open Source is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+See the GNU General Public License for more details. You should have received a copy of the
+GNU General Public License along with Johnny Castaway Open Source. If not, see http://www.gnu.org/licenses/.
+
+*/
+using System;
 using System.Collections.Generic;
 using System.Text;
 
